6aa282f9a731dbc29f85391087cde54cbf8af70b
Fabián <fabian.diartt@mercadolibre.com.co>
2022-12-04 17:44:00 -0500

    feat: create reusable button composable

--- app/src/main/java/dev/fabirt/composeanimationscodelab1/ui/component/HexagonalButton.kt
+++ app/src/main/java/dev/fabirt/composeanimationscodelab1/ui/component/HexagonalButton.kt
@@ -1,2 +1,59 @@
 package dev.fabirt.composeanimationscodelab1.ui.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.fabirt.composeanimationscodelab1.ui.theme.AppTheme
+import dev.fabirt.composeanimationscodelab1.ui.theme.HexagonShape
+import dev.fabirt.composeanimationscodelab1.ui.theme.Orange
+
+@Composable
+fun HexagonalButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    color: Color = Orange,
+    size: Float = DEFAULT_SIZE_DP,
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .size(size.dp)
+            .clip(HexagonShape)
+            .background(color)
+            .clickable {
+                onClick()
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        content()
+    }
+}
+
+private const val DEFAULT_SIZE_DP = 70f
+
+@Composable
+@Preview(name = "Hexagonal Button")
+private fun HexagonalButtonPreview() {
+    AppTheme {
+        HexagonalButton(
+            onClick = {  }
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = null,
+                tint = Color.White
+            )
+        }
+    }
+}
--- app/src/main/java/dev/fabirt/composeanimationscodelab1/ui/theme/Shape.kt
+++ app/src/main/java/dev/fabirt/composeanimationscodelab1/ui/theme/Shape.kt
@@ -1,11 +1,39 @@
 package dev.fabirt.composeanimationscodelab1.ui.theme
 
+import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 val Shapes = Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
     large = RoundedCornerShape(0.dp)
 )
+
+val HexagonShape = GenericShape { size, _ ->
+    val radius = size.width / 2f
+    val angle = 60f
+    var xRef = 0f
+    var yRef = size.width / 2f
+    var sideCount = 0
+    moveTo(xRef, yRef)
+
+    while (sideCount < 5) {
+        val theta = (angle - (angle * sideCount)).toRadians()
+        val x = radius * cos(theta) + xRef
+        val y = radius * sin(theta) + yRef
+
+        lineTo(x, y)
+
+        xRef = x
+        yRef = y
+        sideCount++
+    }
+    close()
+}
+
+fun Float.toRadians(): Float = (this * PI / 180f).toFloat()
