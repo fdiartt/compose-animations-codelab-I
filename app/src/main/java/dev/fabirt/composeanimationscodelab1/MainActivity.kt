46d98fb38220a626840739737aadc9ab9d9c94a3
Fabián <fabian.diartt@mercadolibre.com.co>
2022-12-04 17:11:49 -0500

    refactor: remove unnecessary code

--- app/src/main/java/dev/fabirt/composeanimationscodelab1/MainActivity.kt
+++ app/src/main/java/dev/fabirt/composeanimationscodelab1/MainActivity.kt
@@ -10,31 +10,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.fabirt.composeanimationscodelab1.ui.theme.ComposeAnimationsCodelab1Theme
+import dev.fabirt.composeanimationscodelab1.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeAnimationsCodelab1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            AppTheme {
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeAnimationsCodelab1Theme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/dev/fabirt/composeanimationscodelab1/ui/theme/Color.kt
+++ app/src/main/java/dev/fabirt/composeanimationscodelab1/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package dev.fabirt.composeanimationscodelab1.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val White = Color(0xFFFFFFFF)
+val DarkGray = Color(0xFF333333)
+val Orange = Color(0xFFFF9900)
+val Alto = Color(0xFFD2D2D2)
