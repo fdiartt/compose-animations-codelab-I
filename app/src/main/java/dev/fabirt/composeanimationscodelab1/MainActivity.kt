cfd650db19e0d50778d7920817ac4f00e82e58f6
Fabián <fabian.diartt@mercadolibre.com.co>
2022-12-04 20:37:46 -0500

    feat: final part

--- app/src/main/java/dev/fabirt/composeanimationscodelab1/MainActivity.kt
+++ app/src/main/java/dev/fabirt/composeanimationscodelab1/MainActivity.kt
@@ -3,21 +3,53 @@ package dev.fabirt.composeanimationscodelab1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import dev.fabirt.composeanimationscodelab1.ui.component.AnimatedMenu
+import dev.fabirt.composeanimationscodelab1.ui.model.dummyMenuItems
+import dev.fabirt.composeanimationscodelab1.ui.state.rememberSwitchableState
 import dev.fabirt.composeanimationscodelab1.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppTheme {
+            App()
+        }
+    }
+}
 
+@Composable
+fun App() {
+    AppTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "Compose Animations I") }
+                )
+            },
+            backgroundColor = Color(0xFF333333),
+        ) {
+            val menuState = rememberSwitchableState(true)
+
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                AnimatedMenu(
+                    items = dummyMenuItems,
+                    isCollapsed = menuState.isOn,
+                    onChange = {
+                        menuState.toggle()
+                    }
+                )
             }
         }
     }
